47bc662c4a59891e6e102088a7bcb74c881b92e3
Jon Reades <jon@reades.com>
2015-05-15 17:43:04 +0100

    SQL improvements
    Will ultimately parameterise a lot of this, but being able to run all
    of the aggregation right now is more important.

--- SQL/Load/Annual.sql
+++ SQL/Load/Annual.sql
@@ -58,36 +58,36 @@ ORDER BY
 	completion_year
 ;
 
-DROP MATERIALIZED VIEW IF EXISTS landreg.pd2_annual_fct; 
-CREATE MATERIALIZED VIEW pd2_annual_fct AS 
-SELECT 
-	pd2, 
-	property_type_cd, 
-	extract(year FROM completion_dt) AS completion_year,  
-	count(price_int) as n,
-	min(price_int) as min_price_int, 
-	max(price_int) as max_price_int,
-	sum(price_int) as sum_price_int, 
-	avg(price_int) as avg_price_int, 
-	quantile(price_int, 0.25) as lower_quartile, 
-	quantile(price_int, 0.5) as median_price_int,
-	quantile(price_int, 0.75) as upper_quartile, 
-	stddev_samp(price_int) as sd_price_int, 
-	var_samp(price_int) as var_price_int
-FROM 
-	price_paid_fct AS pf, 
-	pc_mapping_dim AS pmd 
-WHERE 
-	pmd.pc=pf.pc 
-GROUP BY 
-	pd2, 
-	property_type_cd, 
-	completion_year 
-ORDER BY 
-	pd2, 
-	property_type_cd, 
-	completion_year
-;
+-- DROP MATERIALIZED VIEW IF EXISTS landreg.pd2_annual_fct; 
+-- CREATE MATERIALIZED VIEW pd2_annual_fct AS 
+-- SELECT 
+-- 	pd2, 
+-- 	property_type_cd, 
+-- 	extract(year FROM completion_dt) AS completion_year,  
+-- 	count(price_int) as n,
+-- 	min(price_int) as min_price_int, 
+-- 	max(price_int) as max_price_int,
+-- 	sum(price_int) as sum_price_int, 
+-- 	avg(price_int) as avg_price_int, 
+-- 	quantile(price_int, 0.25) as lower_quartile, 
+-- 	quantile(price_int, 0.5) as median_price_int,
+-- 	quantile(price_int, 0.75) as upper_quartile, 
+-- 	stddev_samp(price_int) as sd_price_int, 
+-- 	var_samp(price_int) as var_price_int
+-- FROM 
+-- 	price_paid_fct AS pf, 
+-- 	pc_mapping_dim AS pmd 
+-- WHERE 
+-- 	pmd.pc=pf.pc 
+-- GROUP BY 
+-- 	pd2, 
+-- 	property_type_cd, 
+-- 	completion_year 
+-- ORDER BY 
+-- 	pd2, 
+-- 	property_type_cd, 
+-- 	completion_year
+-- ;
 
 DROP MATERIALIZED VIEW IF EXISTS landreg.pd1_annual_fct; 
 CREATE MATERIALIZED VIEW pd1_annual_fct AS 
@@ -151,36 +151,36 @@ ORDER BY
 	completion_year
 ;
 
-DROP MATERIALIZED VIEW IF EXISTS landreg.pca_annual_fct; 
-CREATE MATERIALIZED VIEW pca_annual_fct AS 
-SELECT 
-	pca, 
-	property_type_cd, 
-	extract(year FROM completion_dt) AS completion_year,  
-	count(price_int) as n,
-	min(price_int) as min_price_int, 
-	max(price_int) as max_price_int,
-	sum(price_int) as sum_price_int, 
-	avg(price_int) as avg_price_int, 
-	quantile(price_int, 0.25) as lower_quartile, 
-	quantile(price_int, 0.5) as median_price_int,
-	quantile(price_int, 0.75) as upper_quartile, 
-	stddev_samp(price_int) as sd_price_int, 
-	var_samp(price_int) as var_price_int
-FROM 
-	price_paid_fct AS pf, 
-	pc_mapping_dim AS pmd 
-WHERE 
-	pmd.pc=pf.pc 
-GROUP BY 
-	pca, 
-	property_type_cd, 
-	completion_year 
-ORDER BY 
-	pca, 
-	property_type_cd, 
-	completion_year
-;
+-- DROP MATERIALIZED VIEW IF EXISTS landreg.pca_annual_fct; 
+-- CREATE MATERIALIZED VIEW pca_annual_fct AS 
+-- SELECT 
+-- 	pca, 
+-- 	property_type_cd, 
+-- 	extract(year FROM completion_dt) AS completion_year,  
+-- 	count(price_int) as n,
+-- 	min(price_int) as min_price_int, 
+-- 	max(price_int) as max_price_int,
+-- 	sum(price_int) as sum_price_int, 
+-- 	avg(price_int) as avg_price_int, 
+-- 	quantile(price_int, 0.25) as lower_quartile, 
+-- 	quantile(price_int, 0.5) as median_price_int,
+-- 	quantile(price_int, 0.75) as upper_quartile, 
+-- 	stddev_samp(price_int) as sd_price_int, 
+-- 	var_samp(price_int) as var_price_int
+-- FROM 
+-- 	price_paid_fct AS pf, 
+-- 	pc_mapping_dim AS pmd 
+-- WHERE 
+-- 	pmd.pc=pf.pc 
+-- GROUP BY 
+-- 	pca, 
+-- 	property_type_cd, 
+-- 	completion_year 
+-- ORDER BY 
+-- 	pca, 
+-- 	property_type_cd, 
+-- 	completion_year
+-- ;
 
 -- Region
 DROP MATERIALIZED VIEW IF EXISTS landreg.region_annual_fct; 
@@ -270,33 +270,33 @@ ORDER BY
 	completion_year
 ;
 
-DROP MATERIALIZED VIEW IF EXISTS landreg.pd2_annual_change_fct; 
-CREATE MATERIALIZED VIEW pd2_annual_change_fct AS 
-SELECT 
-	pd2,
-	extract(year FROM completion_dt) AS completion_year,  
-	count(price_int) as n,
-	min(price_int) as min_price_int, 
-	max(price_int) as max_price_int,
-	sum(price_int) as sum_price_int, 
-	avg(price_int) as avg_price_int, 
-	quantile(price_int, 0.25) as lower_quartile, 
-	quantile(price_int, 0.5) as median_price_int,
-	quantile(price_int, 0.75) as upper_quartile, 
-	stddev_samp(price_int) as sd_price_int, 
-	var_samp(price_int) as var_price_int
-FROM 
-	price_paid_fct AS pf, 
-	pc_mapping_dim AS pmd 
-WHERE 
-	pmd.pc=pf.pc 
-GROUP BY 
-	pd2,  
-	completion_year 
-ORDER BY 
-	pd2,  
-	completion_year
-;
+-- DROP MATERIALIZED VIEW IF EXISTS landreg.pd2_annual_change_fct; 
+-- CREATE MATERIALIZED VIEW pd2_annual_change_fct AS 
+-- SELECT 
+-- 	pd2,
+-- 	extract(year FROM completion_dt) AS completion_year,  
+-- 	count(price_int) as n,
+-- 	min(price_int) as min_price_int, 
+-- 	max(price_int) as max_price_int,
+-- 	sum(price_int) as sum_price_int, 
+-- 	avg(price_int) as avg_price_int, 
+-- 	quantile(price_int, 0.25) as lower_quartile, 
+-- 	quantile(price_int, 0.5) as median_price_int,
+-- 	quantile(price_int, 0.75) as upper_quartile, 
+-- 	stddev_samp(price_int) as sd_price_int, 
+-- 	var_samp(price_int) as var_price_int
+-- FROM 
+-- 	price_paid_fct AS pf, 
+-- 	pc_mapping_dim AS pmd 
+-- WHERE 
+-- 	pmd.pc=pf.pc 
+-- GROUP BY 
+-- 	pd2,  
+-- 	completion_year 
+-- ORDER BY 
+-- 	pd2,  
+-- 	completion_year
+-- ;
 
 DROP MATERIALIZED VIEW IF EXISTS landreg.pd1_annual_change_fct; 
 CREATE MATERIALIZED VIEW pd1_annual_change_fct AS 
@@ -354,33 +354,33 @@ ORDER BY
 	completion_year
 ;
 
-DROP MATERIALIZED VIEW IF EXISTS landreg.pca_annual_change_fct; 
-CREATE MATERIALIZED VIEW pca_annual_change_fct AS 
-SELECT 
-	pca,
-	extract(year FROM completion_dt) AS completion_year,  
-	count(price_int) as n,
-	min(price_int) as min_price_int, 
-	max(price_int) as max_price_int,
-	sum(price_int) as sum_price_int, 
-	avg(price_int) as avg_price_int, 
-	quantile(price_int, 0.25) as lower_quartile, 
-	quantile(price_int, 0.5) as median_price_int,
-	quantile(price_int, 0.75) as upper_quartile, 
-	stddev_samp(price_int) as sd_price_int, 
-	var_samp(price_int) as var_price_int
-FROM 
-	price_paid_fct AS pf, 
-	pc_mapping_dim AS pmd 
-WHERE 
-	pmd.pc=pf.pc 
-GROUP BY 
-	pca, 
-	completion_year 
-ORDER BY 
-	pca, 
-	completion_year
-;
+-- DROP MATERIALIZED VIEW IF EXISTS landreg.pca_annual_change_fct; 
+-- CREATE MATERIALIZED VIEW pca_annual_change_fct AS 
+-- SELECT 
+-- 	pca,
+-- 	extract(year FROM completion_dt) AS completion_year,  
+-- 	count(price_int) as n,
+-- 	min(price_int) as min_price_int, 
+-- 	max(price_int) as max_price_int,
+-- 	sum(price_int) as sum_price_int, 
+-- 	avg(price_int) as avg_price_int, 
+-- 	quantile(price_int, 0.25) as lower_quartile, 
+-- 	quantile(price_int, 0.5) as median_price_int,
+-- 	quantile(price_int, 0.75) as upper_quartile, 
+-- 	stddev_samp(price_int) as sd_price_int, 
+-- 	var_samp(price_int) as var_price_int
+-- FROM 
+-- 	price_paid_fct AS pf, 
+-- 	pc_mapping_dim AS pmd 
+-- WHERE 
+-- 	pmd.pc=pf.pc 
+-- GROUP BY 
+-- 	pca, 
+-- 	completion_year 
+-- ORDER BY 
+-- 	pca, 
+-- 	completion_year
+-- ;
 
 -- Region
 DROP MATERIALIZED VIEW IF EXISTS landreg.region_annual_change_fct; 
@@ -416,10 +416,11 @@ order by
 -- all together into one column per year so 
 -- it's easy to calculate change in one go.
 -- 
-
+DROP MATERIALIZED VIEW IF EXISTS landreg.pd_year_on_year_fct; 
+CREATE MATERIALIZED VIEW pd_year_on_year_fct AS 
 SELECT 
 	p.gid, 
-	p.pca, 
+	p.pd, 
 	p.geom, 
 	pf1.avg_price_int as avg1995, 
 	pf1.median_price_int as med1995, 
@@ -462,24 +463,24 @@ SELECT
 	pf20.avg_price_int as avg2014, 
 	pf20.median_price_int as med2014
 FROM 
-	os.pca_spa as p 
-LEFT OUTER JOIN pca_annual_change_fct pf1  ON (p.pca=pf1.pca)  WHERE pf1.completion_year=1995 
-LEFT OUTER JOIN pca_annual_change_fct pf2  ON (p.pca=pf2.pca)  WHERE pf2.completion_year=1996 
-LEFT OUTER JOIN pca_annual_change_fct pf3  ON (p.pca=pf3.pca)  WHERE pf3.completion_year=1997 
-LEFT OUTER JOIN pca_annual_change_fct pf4  ON (p.pca=pf4.pca)  WHERE pf4.completion_year=1998 
-LEFT OUTER JOIN pca_annual_change_fct pf5  ON (p.pca=pf5.pca)  WHERE pf5.completion_year=1999 
-LEFT OUTER JOIN pca_annual_change_fct pf6  ON (p.pca=pf6.pca)  WHERE pf6.completion_year=2000 
-LEFT OUTER JOIN pca_annual_change_fct pf7  ON (p.pca=pf7.pca)  WHERE pf7.completion_year=2001 
-LEFT OUTER JOIN pca_annual_change_fct pf8  ON (p.pca=pf8.pca)  WHERE pf8.completion_year=2002 
-LEFT OUTER JOIN pca_annual_change_fct pf9  ON (p.pca=pf9.pca)  WHERE pf9.completion_year=2003 
-LEFT OUTER JOIN pca_annual_change_fct pf10 ON (p.pca=pf10.pca) WHERE pf10.completion_year=2004 
-LEFT OUTER JOIN pca_annual_change_fct pf11 ON (p.pca=pf11.pca) WHERE pf11.completion_year=2005 
-LEFT OUTER JOIN pca_annual_change_fct pf12 ON (p.pca=pf12.pca) WHERE pf12.completion_year=2006 
-LEFT OUTER JOIN pca_annual_change_fct pf13 ON (p.pca=pf13.pca) WHERE pf13.completion_year=2007 
-LEFT OUTER JOIN pca_annual_change_fct pf14 ON (p.pca=pf14.pca) WHERE pf14.completion_year=2008 
-LEFT OUTER JOIN pca_annual_change_fct pf15 ON (p.pca=pf15.pca) WHERE pf15.completion_year=2009 
-LEFT OUTER JOIN pca_annual_change_fct pf16 ON (p.pca=pf16.pca) WHERE pf16.completion_year=2010 
-LEFT OUTER JOIN pca_annual_change_fct pf17 ON (p.pca=pf17.pca) WHERE pf17.completion_year=2011 
-LEFT OUTER JOIN pca_annual_change_fct pf18 ON (p.pca=pf18.pca) WHERE pf18.completion_year=2012 
-LEFT OUTER JOIN pca_annual_change_fct pf19 ON (p.pca=pf19.pca) WHERE pf19.completion_year=2013 
-LEFT OUTER JOIN pca_annual_change_fct pf20 ON (p.pca=pf20.pca) WHERE pf20.completion_year=2014 
+	os.pd_spa as p 
+LEFT OUTER JOIN pd_annual_change_fct pf1  ON (p.pd=pf1.pd)  AND pf1.completion_year=1995 
+LEFT OUTER JOIN pd_annual_change_fct pf2  ON (p.pd=pf2.pd)  AND pf2.completion_year=1996 
+LEFT OUTER JOIN pd_annual_change_fct pf3  ON (p.pd=pf3.pd)  AND pf3.completion_year=1997 
+LEFT OUTER JOIN pd_annual_change_fct pf4  ON (p.pd=pf4.pd)  AND pf4.completion_year=1998 
+LEFT OUTER JOIN pd_annual_change_fct pf5  ON (p.pd=pf5.pd)  AND pf5.completion_year=1999 
+LEFT OUTER JOIN pd_annual_change_fct pf6  ON (p.pd=pf6.pd)  AND pf6.completion_year=2000 
+LEFT OUTER JOIN pd_annual_change_fct pf7  ON (p.pd=pf7.pd)  AND pf7.completion_year=2001 
+LEFT OUTER JOIN pd_annual_change_fct pf8  ON (p.pd=pf8.pd)  AND pf8.completion_year=2002 
+LEFT OUTER JOIN pd_annual_change_fct pf9  ON (p.pd=pf9.pd)  AND pf9.completion_year=2003 
+LEFT OUTER JOIN pd_annual_change_fct pf10 ON (p.pd=pf10.pd) AND pf10.completion_year=2004 
+LEFT OUTER JOIN pd_annual_change_fct pf11 ON (p.pd=pf11.pd) AND pf11.completion_year=2005 
+LEFT OUTER JOIN pd_annual_change_fct pf12 ON (p.pd=pf12.pd) AND pf12.completion_year=2006 
+LEFT OUTER JOIN pd_annual_change_fct pf13 ON (p.pd=pf13.pd) AND pf13.completion_year=2007 
+LEFT OUTER JOIN pd_annual_change_fct pf14 ON (p.pd=pf14.pd) AND pf14.completion_year=2008 
+LEFT OUTER JOIN pd_annual_change_fct pf15 ON (p.pd=pf15.pd) AND pf15.completion_year=2009 
+LEFT OUTER JOIN pd_annual_change_fct pf16 ON (p.pd=pf16.pd) AND pf16.completion_year=2010 
+LEFT OUTER JOIN pd_annual_change_fct pf17 ON (p.pd=pf17.pd) AND pf17.completion_year=2011 
+LEFT OUTER JOIN pd_annual_change_fct pf18 ON (p.pd=pf18.pd) AND pf18.completion_year=2012 
+LEFT OUTER JOIN pd_annual_change_fct pf19 ON (p.pd=pf19.pd) AND pf19.completion_year=2013 
+LEFT OUTER JOIN pd_annual_change_fct pf20 ON (p.pd=pf20.pd) AND pf20.completion_year=2014;
